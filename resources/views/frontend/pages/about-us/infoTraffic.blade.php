d3df48ab1b2afd07e8a85523f0550ab8ff211a14
Rachmat Hidayat <rachmth.059@gmail.com>
2022-06-11 00:00:20 +0800

    multi-page-version

--- resources/views/frontend/pages/about-us/infoTraffic.blade.php
+++ resources/views/frontend/pages/about-us/infoTraffic.blade.php
@@ -5,16 +5,17 @@
 
 <br>
 <div class="container p-0">
-    <br>
-    @include('frontend.pages.about-us.chart-section.section1')
-    <br>
-    @include('frontend.pages.about-us.chart-section.section2')
-    <br>
-    @include('frontend.pages.about-us.chart-section.section3')
-    <br>
-    @include('frontend.pages.about-us.chart-section.section4')
-    <br>
-    @include('frontend.pages.about-us.chart-section.section5')
+    @if (route('mmn-harian') || route('jtse-harian'))
+        @include('frontend.pages.about-us.chart-section.section1')
+    @elseif (route('mmn-bulanan') || route('jtse-bulanan'))
+        @include('frontend.pages.about-us.chart-section.section2')
+    @elseif (route('mmn-gerbang-harian') || route('jtse-gerbang-harian'))
+        @include('frontend.pages.about-us.chart-section.section3')
+    @elseif (route('mmn-komposisi') || route('jtse-komposisi'))
+         @include('frontend.pages.about-us.chart-section.section4')
+    @elseif (route('mmn-traffic-history') || route('jtse-traffic-history'))
+        @include('frontend.pages.about-us.chart-section.section5')
+    @endif
 </div>
 
 
