a283289f8f6136bbc8bd942addb83ddd21dbdb98
isarYMB <yizchard0897@gmail.com>
2022-06-24 01:16:29 +0800

    complete

--- crudbooster/src/views/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-{{ cbLang('right') }} hidden-xs">
-        <div>Nusantara Infrastructer</div>
-    </div>
-    <!-- Default to the left -->
-    <strong>{{ cbLang('copyright') }} &copy; <?php echo date('Y') ?>. {{ cbLang('all_rights_reserved') }} .</strong>
-</footer>
